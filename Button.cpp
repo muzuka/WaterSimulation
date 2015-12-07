b096a5b28a200d0b380def3976cc041ac9a295c8
muzuka <seanbrown7935@gmail.com>
2015-12-07 15:08:56 -0700

    indented

--- Button.cpp
+++ Button.cpp
@@ -37,11 +37,11 @@ void Button::setColor(Vector c) {
 void Button::render() {
   
 	glBegin(GL_TRIANGLE_FAN);
-	glColor3f(1.0f, 1.0f, 1.0f);
-	glVertex3f(bottomLeft.getX(), bottomLeft.getY(), bottomLeft.getZ());
-	glVertex3f(bottomLeft.getX(), bottomLeft.getY() + height, bottomLeft.getZ());
-	glVertex3f(bottomLeft.getX() + width, bottomLeft.getY(), bottomLeft.getZ());
-	glVertex3f(topRight.getX(), topRight.getY(), topRight.getZ());
-  glVertex3f(bottomLeft.getX(), bottomLeft.getY(), bottomLeft.getZ());
+		glColor3f(1.0f, 1.0f, 1.0f);
+		glVertex3f(bottomLeft.getX(), bottomLeft.getY(), bottomLeft.getZ());
+		glVertex3f(bottomLeft.getX(), bottomLeft.getY() + height, bottomLeft.getZ());
+		glVertex3f(bottomLeft.getX() + width, bottomLeft.getY(), bottomLeft.getZ());
+		glVertex3f(topRight.getX(), topRight.getY(), topRight.getZ());
+	  	glVertex3f(bottomLeft.getX(), bottomLeft.getY(), bottomLeft.getZ());
 	glEnd();
 }
